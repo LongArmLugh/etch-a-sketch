2bd93c2aaf1e4b50077f2c73e15e98650078fbd9
Lugh_Lámfada <healingphases@gmail.com>
2021-12-05 12:27:23 -0800

    Revert "finished first version"
    This reverts commit 9129c4cd53e9ccab82b8cb6306a9fc705d962b7a.

--- scripts.js
+++ scripts.js
@@ -1,43 +1,11 @@
 //Make grid programatically
 const sketchBox = document.getElementById('sketch-box');
-var resolution = 16;
 
-// sketchBox.style.gridTemplateColumns = `repeat($s{resolution}, 1fr)`;
+let n = 16;
 
-makeCanvas();
+sketchBox.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
 
-function makeCanvas() {
-    sketchBox.style.gridTemplateColumns = `repeat(${resolution}, 1fr)`;
-    for (let i = 0; i < resolution * resolution; i++) {
-    const pixel = document.createElement('div');
-    pixel.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "rgb(0, 0, 0, 0.66)";
-    });
-    sketchBox.append(pixel);
-    }
+for (let i = 0; i < n * n; i++) {
+    const pixelBox = document.createElement('div');
+    sketchBox.append(pixelBox);
 }
-
-
-// const pixels = sketchBox.children;
-
-function reset() {
-    sketchBox.querySelectorAll('*').forEach( n => n.remove() );
-    resolution = setResolution(); 
-    sketchBox.style.gridTemplateColumns = `repeat(${resolution}, 1fr)`; 
-    makeCanvas();
-}
-
-function setResolution() {
-    let n = prompt("resolution value 16 to 100");
-    if (n > 100) {
-        alert('resolution set to max(100)');
-        n = 100;
-    }
-    if (n < 16) {
-        alert("resolution set to min(16)");
-        n = 16;
-    }
-    return n;
-}
-
-
